4591cb691c001dcf52fbe038d812df45e2a23bea
Kamiche Mohamed Raouf <93711354+Raoufkam@users.noreply.github.com>
2022-04-17 04:35:11 +0200

    Add files via upload

---
+++ couche1.h
@@ -0,0 +1,27 @@
+/********************************************************************
+ *    Couche 1 : les blocs                                          *
+ * 	  Gestion de l'écriture d'un bloc sur le disque                 *
+ * 	  Projet S4                                                     *
+ *                                                                  *
+ * 	  K.Mohamed - Univ. Toulouse III Paul-Sabatier 2021-2022        *
+ *                                                                  *
+ /******************************************************************/
+
+#ifndef couche1_h
+#define couche1_h
+
+
+#include "sos_defines.h"
+#include "headers.h"
+#include "include.h"
+
+
+/* Initialise la variable 'virtual_disk_sos' à partir du nom du répertoire */
+void init_disk_sos(char* nom_repertoire) ;
+
+
+/* Fonction qui veille a ce que le système soit éteint proprement : Sauvgarde du catalogue */
+void eteindreSys(FILE* vdisk);
+
+/* Calcule le nombre de blocs nécessaires pour stocker un nombre n_octets */
+unsigned int compute_nblock(unsigned int n_octets);
