ee98ce3e9af7dbb9e98e1480864cc6bb1a430555
Kamiche Mohamed Raouf <93711354+Raoufkam@users.noreply.github.com>
2022-04-17 17:57:44 +0200

    Update couche1.c

--- couche1.c
+++ couche1.c
@@ -1,31 +1,26 @@
 /********************************************************************
  *    Couche 1 : les blocs                                          *
- *    Gestion de l'écriture d'un bloc sur le disque                 *
- *    Projet S4                                                     *
+ * 	  Gestion de l'écriture d'un bloc sur le disque                 *
+ * 	  Projet S4                                                     *
  *                                                                  *
- *    K.Mohamed - Univ. Toulouse III Paul-Sabatier  2021-2022       *
+ * 	  K.Mohamed - Univ. Toulouse III Paul-Sabatier   2021-2022      *
  *                                                                  *
- /******************************************************************/
+ ********************************************************************/
  
  
-#include "headers.h"
-#include "include.h"
+#include "couche1.h"
 
 
-virtual_disk_t virtual_disk_sos ;
-
-void init_disk_sos(char* nom_repertoire)
+void init_disk_sos(char* nom_repertoire,virtual_disk_t virtual_disk_sos)
 {
-
     char* file_emplacement = "./";
     strcat(file_emplacement,nom_repertoire);
     strcat(file_emplacement,"/d0");
     virtual_disk_sos.storage = fopen(file_emplacement,"ab+");
-    virtual_disk_sos.super_block = {(uint)0,(uint)0,(uint)0,(uint)0};
+    //virtual_disk_sos.super_block = {(uint)0,(uint)0,(uint)0,(uint)0};
     /* A voir
     virtual_disk_sos.users_table
     */
-
 }
 
 void eteindreSys(FILE* vdisk)
@@ -57,7 +52,7 @@ unsigned int compute_nblock(unsigned int n_octets){
 				n_octets= n_octets - 4 ;
 			}
 		}
-		if (nb_octets == 2) {
+		if (n_octets == 2) {
 			/* Dans le cas ou le nombre d'octets = 2 octets -> 1 blocs */
 			nb_blocs++ ;
 		}
@@ -90,17 +85,17 @@ unsigned int compute_nblock(unsigned int n_octets){
 }
 
 
-void write_block(long pos){
+void write_block(long pos,virtual_disk_t virtual_disk_sos){
 	
 	/**
 	 *  On commence par positioner le curseur à la position donnée en paramètre
 	 * 	-> On considère que le fichier du virtual_disk_sos est ouvert dans init_disk_sos(char* nom_repertoire) 
 	 */
 	 
-	int position = NULL ;
+	int position = 0 ;
 	position = fseek(virtual_disk_sos.storage,pos,SEEK_SET);
 	if (position != 0) {
-		fprintf(stderr, "Position impossible");
+		fprintf(stderr, "Position impossible - fonction ecriture\n");
 		eteindreSys(virtual_disk_sos.storage);
 		exit(EXIT_FAILURE);
 	}
@@ -110,9 +105,9 @@ void write_block(long pos){
 	 */
 	 
 	block_t buffer ;
-	if (fwrite(buffer,BLOCK_SIZE,1,virtual_disk_sos.storage )!=1) {
+	if (fwrite(buffer.data,BLOCK_SIZE,1,virtual_disk_sos.storage )!=1) {
 		fprintf(stderr, "Ecriture du bloc impossible dans le fichier\n");
-		eteindreSys(virtual_disk_sos);
+		eteindreSys(virtual_disk_sos.storage);
 		exit(EXIT_FAILURE);
 	}
 	
@@ -123,3 +118,28 @@ void write_block(long pos){
 	 
 	 eteindreSys(virtual_disk_sos.storage);
 }
+
+
+void read_block(long pos,virtual_disk_t virtual_disk_sos){
+	
+	/**
+	 *  On commence par positioner le curseur à la psotion 'pos'
+	 *  -> On considère que le fichier du virtual_disk_sos est ouvert dans init_disk_sos(char* nom_repertoire) 
+	 */
+	
+	int position = 0 ;
+	position = fseek(virtual_disk_sos.storage,pos,SEEK_SET);
+	if (position != 0) {
+		fprintf(stderr, "Position impossible - fonction lecture\n");
+		eteindreSys(virtual_disk_sos.storage);
+		exit(EXIT_FAILURE);
+	}
+	
+	block_t buffer ;
+	if (fread(buffer.data,BLOCK_SIZE,1,virtual_disk_sos.storage )!=1) {
+		fprintf(stderr, "Lecture du bloc impossible dans le fichier\n");
+		eteindreSys(virtual_disk_sos.storage);
+		exit(EXIT_FAILURE);
+	}
+}
+
