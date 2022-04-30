5701a5d4699b3de46f717bbbfb7b1377ab920ab4
Pial0 <94445337+Pial0@users.noreply.github.com>
2022-04-30 15:14:26 +0200

    Add files via upload

--- couche1.c
+++ couche1.c
@@ -16,7 +16,7 @@ void init_disk_sos(char* nom_repertoire,virtual_disk_t virtual_disk_sos)
     char* file_emplacement = "./";
     strcat(file_emplacement,nom_repertoire);
     strcat(file_emplacement,"/d0");
-    virtual_disk_sos.storage = fopen(file_emplacement,"ab+");
+    virtual_disk_sos.storage = fopen(file_emplacement,"rb+");
     //virtual_disk_sos.super_block = {(uint)0,(uint)0,(uint)0,(uint)0};
     /* A voir
     virtual_disk_sos.users_table
@@ -30,9 +30,9 @@ void eteindreSys(FILE* vdisk)
 
 }
 
-unsigned int compute_nblock(unsigned int n_octets){
+unsigned int compute_nblock(uchar n_octets){
 	
-	unsigned int nb_blocs = 0 ;
+	uchar nb_blocs = 0 ;
 	
 	
 	if ( n_octets == 0 ){
@@ -40,47 +40,10 @@ unsigned int compute_nblock(unsigned int n_octets){
 		return  0 ;
 	}
 	
-	/**
-	 * Dans le cas ou le nombre d'octets est pairs
-	 */
-	 
-	if (n_octets%2==0 ){
-		/* Des blocs de 4 octets */
-		if ( n_octets >= 4 ){
-			while ( n_octets!=0 ){
-				nb_blocs++ ;
-				n_octets= n_octets - 4 ;
-			}
-		}
-		if (n_octets == 2) {
-			/* Dans le cas ou le nombre d'octets = 2 octets -> 1 blocs */
-			nb_blocs++ ;
-		}
+	while ( n_octets > 0 ){
+		nb_blocs++ ;
+		n_octets= n_octets - BLOCK_SIZE;
 	}
-		 
-	
-	/**
-	 * Dans le cas ou le nombre d'octets est impairs
-	 */
-	 
-	 if (n_octets%2!=0 ){
-		 if ( n_octets > 4 ){
-			 int q = n_octets / 4 ;
-			 /* Des blocs de 4 octets dans n octets */
-			 for(int i=0 ; i < q ; i++){
-				 nb_blocs++ ;
-			 }
-			 /* Ajouter un bloc pour les octets qui reste ' inférieur à 4 octets '*/
-			 nb_blocs++;
-		}
-		
-		else {
-			/* On les mets dans un seul bloc de n octets */
-			nb_blocs++;
-		}
-	}
-	
-	
 	return nb_blocs ;
 }
 
@@ -111,12 +74,6 @@ void write_block(long pos,virtual_disk_t virtual_disk_sos){
 		exit(EXIT_FAILURE);
 	}
 	
-	
-	/**
-	 *  On doit fermer le fichier du système après la fin de l'écriture du bloc
-	 */
-	 
-	 eteindreSys(virtual_disk_sos.storage);
 }
 
 
