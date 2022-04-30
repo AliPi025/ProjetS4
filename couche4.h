ed08c36f56923842b72d36267e0d086cfdeb908d
Kamiche Mohamed Raouf <93711354+Raoufkam@users.noreply.github.com>
2022-04-30 16:55:05 +0200

    Update couche4.h

--- couche4.h
+++ couche4.h
@@ -1,9 +1,9 @@
 /********************************************************************
- *    Couche 4 : Fichiers                                           *
- * 	  Gestion des fichiers et des droits                            *
- * 	  Projet S4                                                     *
+ *    Couche 4 : les blocs                                          *
+ *    Gestion de l'écriture d'un bloc sur le disque                 *
+ *    Projet S4                                                     *
  *                                                                  *
- * 	  K.Mohamed - Univ. Toulouse III Paul-Sabatier   2021-2022      *
+ *    K.Mohamed - Univ. Toulouse III Paul-Sabatier   2021-2022      *
  *                                                                  *
  ********************************************************************/
 
@@ -11,7 +11,7 @@
 #define couche4_h
 
 /* Fonction qui écrit le fichier 'fichier' sur le système */
-write_file(char* Nomfichier,file_t fichier)
+void write_file(char* Nomfichier,file_t fichier)
 
 
 #include "sos_defines.h"
